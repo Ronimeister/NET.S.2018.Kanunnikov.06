277b639f164cf96305c5f122ae9495c3aaaff04d
Ronimeister <ronimeisterhs@gmail.com>
2018-07-10 00:14:20 +0300

    Comments added. GetHashCode will be added soon.

--- PolynomialLib/Polynomial.cs
+++ PolynomialLib/Polynomial.cs
@@ -19,10 +19,19 @@ namespace PolynomialLib
         #region ICloneable methods
         object ICloneable.Clone() => Clone();
 
+        /// <summary>
+        /// Provides functionality to clone <see cref="Polynomial"/> object.
+        /// </summary>
+        /// <returns>Cloned <see cref="Polynomial"/> object.</returns>
         public Polynomial Clone() => new Polynomial(_coefficients);
         #endregion
 
         #region IEquatable
+        /// <summary>
+        /// Realization of IQuetable.Equals(). This method checked two <see cref="Polynomial"/> objects for equality.
+        /// </summary>
+        /// <param name="other">The <see cref="Polynomial"/> object need to be checked.</param>
+        /// <returns>Bool value.</returns>
         public bool Equals(Polynomial other)
         {
             if (ReferenceEquals(this, other))
@@ -58,6 +67,9 @@ namespace PolynomialLib
 
         private static readonly double ComparisonEpsilon;
 
+        /// <summary>
+        /// Public property that return the length of the Polynomial.
+        /// </summary>
         public int Length
         {
             get
@@ -66,6 +78,9 @@ namespace PolynomialLib
             }
         }
 
+        /// <summary>
+        /// Public property that return the max power of the Polynomial.
+        /// </summary>
         public int Power
         {
             get
@@ -96,6 +111,9 @@ namespace PolynomialLib
                 coefficients.CopyTo(_coefficients, 0);
             }
 
+        /// <summary>
+        /// Static constructor for Polynomial class that provides functionality to change <see cref="ComparisonEpsilon"/> value.
+        /// </summary>
         static Polynomial()
         {
             try
@@ -231,6 +249,14 @@ namespace PolynomialLib
         #endregion
 
         #region Public methods
+        /// <summary>
+        /// This method summing two <see cref="Polynomial"/> objects.
+        /// </summary>
+        /// <param name="lhs">First object need to be summed.</param>
+        /// <param name="rhs">Second object need to be summed.</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="lhs"/> or <paramref name="rhs"/> is equal to null.</exception>
+        /// <exception cref="ArgumentException">Throws when <paramref name="lhs"/> or <paramref name="rhs"/> Length property is equal to 0.</exception>
+        /// <returns>New <see cref="Polynomial"/> object.</returns>
         public static Polynomial Add(Polynomial lhs, Polynomial rhs)
         {
             CheckInput(lhs, rhs);
@@ -238,6 +264,14 @@ namespace PolynomialLib
             return Sum(lhs, rhs);
         }
 
+        /// <summary>
+        /// This method summing <see cref="Polynomial"/> object and <see cref="double"/> value.
+        /// </summary>
+        /// <param name="lhs"><see cref="Polynomial"/> object need to be summed.</param>
+        /// <param name="rhs"><see cref="double"/> value need to be summed.</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="lhs"/> is equal to null.</exception>
+        /// <exception cref="ArgumentException">Throws when <paramref name="lhs"/> Length property is equal to 0.</exception> 
+        /// <returns>New <see cref="Polynomial"/> object.</returns>
         public static Polynomial Add(Polynomial lhs, double rhs)
         {
             CheckInput(lhs);
@@ -245,6 +279,14 @@ namespace PolynomialLib
             return Sum(lhs, rhs);
         }
 
+        /// <summary>
+        /// This method summing <see cref="double"/> value and <see cref="Polynomial"/> object.
+        /// </summary>
+        /// <param name="lhs"><see cref="double"/> value need to be summed.</param>
+        /// <param name="rhs"><see cref="Polynomial"/> object need to be summed.</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="rhs"/> is equal to null.</exception>
+        /// <exception cref="ArgumentException">Throws when <paramref name="rhs"/> Length property is equal to 0.</exception> 
+        /// <returns>New <see cref="Polynomial"/> object.</returns>
         public static Polynomial Add(double lhs, Polynomial rhs)
         {
             CheckInput(rhs);
@@ -252,6 +294,14 @@ namespace PolynomialLib
             return Sum(lhs, rhs);
         }
 
+        /// <summary>
+        /// This method subtracting two <see cref="Polynomial"/> objects.
+        /// </summary>
+        /// <param name="lhs">First object need to be subract.</param>
+        /// <param name="rhs">Second object need to be subract.</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="lhs"/> or <paramref name="rhs"/> is equal to null.</exception>
+        /// <exception cref="ArgumentException">Throws when <paramref name="lhs"/> or <paramref name="rhs"/> Length property is equal to 0.</exception>
+        /// <returns>New <see cref="Polynomial"/> object.</returns>
         public static Polynomial Subract(Polynomial lhs, Polynomial rhs)
         {
             CheckInput(lhs, rhs);
@@ -259,6 +309,14 @@ namespace PolynomialLib
             return Subtr(lhs, rhs);
         }
 
+        /// <summary>
+        /// This method subtracting <see cref="Polynomial"/> object and <see cref="double"/> value.
+        /// </summary>
+        /// <param name="lhs"><see cref="Polynomial"/> object need to be subract.</param>
+        /// <param name="rhs"><see cref="double"/> value need to be subract.</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="lhs"/> is equal to null.</exception>
+        /// <exception cref="ArgumentException">Throws when <paramref name="lhs"/> Length property is equal to 0.</exception> 
+        /// <returns>New <see cref="Polynomial"/> object.</returns>
         public static Polynomial Subract(Polynomial lhs, double rhs)
         {
             CheckInput(lhs);
@@ -266,6 +324,14 @@ namespace PolynomialLib
             return Subtr(lhs, rhs);
         }
 
+        /// <summary>
+        /// This method subtracting <see cref="double"/> value and <see cref="Polynomial"/> object.
+        /// </summary>
+        /// <param name="lhs"><see cref="double"/> value need to be subract.</param>
+        /// <param name="rhs"><see cref="Polynomial"/> object need to be subract.</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="rhs"/> is equal to null.</exception>
+        /// <exception cref="ArgumentException">Throws when <paramref name="rhs"/> Length property is equal to 0.</exception> 
+        /// <returns>New <see cref="Polynomial"/> object.</returns>
         public static Polynomial Subract(double lhs, Polynomial rhs)
         {
             CheckInput(rhs);
@@ -273,6 +339,14 @@ namespace PolynomialLib
             return Subtr(lhs, rhs);
         }
 
+        /// <summary>
+        /// This method multiplying two <see cref="Polynomial"/> objects.
+        /// </summary>
+        /// <param name="lhs">First object need to be multiplied.</param>
+        /// <param name="rhs">Second object need to be multiplied.</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="lhs"/> or <paramref name="rhs"/> is equal to null.</exception>
+        /// <exception cref="ArgumentException">Throws when <paramref name="lhs"/> or <paramref name="rhs"/> Length property is equal to 0.</exception>
+        /// <returns>New <see cref="Polynomial"/> object.</returns>
         public static Polynomial Multiply(Polynomial lhs, Polynomial rhs)
         {
             CheckInput(lhs, rhs);
@@ -280,6 +354,14 @@ namespace PolynomialLib
             return Mult(lhs, rhs);
         }
 
+        /// <summary>
+        /// This method multiplying <see cref="Polynomial"/> object and <see cref="double"/> value.
+        /// </summary>
+        /// <param name="lhs"><see cref="Polynomial"/> object need to be multiplied.</param>
+        /// <param name="rhs"><see cref="double"/> value need to be multiplied.</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="lhs"/> is equal to null.</exception>
+        /// <exception cref="ArgumentException">Throws when <paramref name="lhs"/> Length property is equal to 0.</exception> 
+        /// <returns>New <see cref="Polynomial"/> object.</returns>
         public static Polynomial Multiply(Polynomial lhs, double rhs)
         {
             CheckInput(lhs);
@@ -287,6 +369,14 @@ namespace PolynomialLib
             return Mult(lhs, rhs);
         }
 
+        /// <summary>
+        /// This method multiplying <see cref="double"/> value and <see cref="Polynomial"/> object.
+        /// </summary>
+        /// <param name="lhs"><see cref="double"/> value need to be multiplied.</param>
+        /// <param name="rhs"><see cref="Polynomial"/> object need to be multiplied.</param>
+        /// <exception cref="ArgumentNullException">Throws when <paramref name="rhs"/> is equal to null.</exception>
+        /// <exception cref="ArgumentException">Throws when <paramref name="rhs"/> Length property is equal to 0.</exception> 
+        /// <returns>New <see cref="Polynomial"/> object.</returns>
         public static Polynomial Multiply(double lhs, Polynomial rhs)
         {
             CheckInput(rhs);
@@ -294,6 +384,10 @@ namespace PolynomialLib
             return Mult(lhs, rhs);
         }
 
+        /// <summary>
+        /// This method allows user to get coefficientf of <see cref="Polynomial"/> object as <see cref="double[]"/> array.
+        /// </summary>
+        /// <returns><see cref="double[]"/> array of coefficients.</returns>
         public double[] ToArray()
         {
             double[] result = new double[Length];
@@ -307,12 +401,6 @@ namespace PolynomialLib
         #region Private API
         private bool IsEqualDoubles(double a, double b) => Math.Abs(a - b) < ComparisonEpsilon;
 
-        /// <summary>
-        /// Check input Polynomial object for exceptions.
-        /// </summary>
-        /// <param name="poly">input Polynomial object</param>
-        /// <exception cref="ArgumentNullException">Throws when <paramref name="poly"/> is equal to null.</exception>
-        /// <exception cref="ArgumentException">Throws when <paramref name="poly"/> Length property is equal to 0.</exception>
         private static void CheckInput(Polynomial poly)
         {
             if (poly == null)
@@ -331,13 +419,7 @@ namespace PolynomialLib
             CheckInput(poly1);
             CheckInput(poly2);
         }
-        
-        /// <summary>
-        /// Create double[] array based on <paramref name="lhs"/> and <paramref name="rhs"/> based on their <see cref="Length"/> property.
-        /// </summary>
-        /// <param name="lhs">input double[] array.</param>
-        /// <param name="rhs">input double[] array.</param>
-        /// <returns>New double[] array.</returns>
+
         private static double[] MakePolyArr(double[] lhs, double[] rhs)
             => new double[(lhs.Length > rhs.Length) ? lhs.Length : rhs.Length];
 
